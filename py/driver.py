5550653c519671e707e9f305e4128bee07086c93
Jason Knight <Jason@JasonKnight.us>
2012-08-09 15:16:01 -0500

    Added a better error objective fxn

--- py/driver.py
+++ py/driver.py
@@ -11,7 +11,7 @@ import random
 from time import time
 import pygraphviz as pgv
 import numpy as np
-import pebl as pb
+#import pebl as pb
 #import pstats, cProfile
 
 #cProfile.runctx("b,s=test(); s.sample(10000)", globals(), locals(), "prof.stat")
@@ -34,10 +34,7 @@ def test():
 
     return b, SAMCRun(b)#'db.h5')
 
-def edge_presence(net):
-    return net['matrix'][net['x'][0],net['x'][3]]
-
-def runSAMC(states, data, template, db):
+def runSAMC(states, data, template, db, func):
   nodes = np.arange(data.shape[1])
   b = BayesNet(nodes,states,data,template=template)
   s = SAMCRun(b,db)
@@ -46,7 +43,7 @@ def runSAMC(states, data, template, db):
   t2 = time()
   print("SAMC run took %f seconds." % (t2-t1))
   
-  print s.estimate_func_mean(edge_presence)
+  print s.estimate_func_mean(func)
   t3 = time()
   print("Mean estimation run took %f seconds." % (t3-t2))
   return b, s
@@ -60,38 +57,43 @@ def runFullSAMC(graph, iters, numtemplate, numdata, priorweight, db):
   b,s = runSAMC(states, traindata, tmat, db)
   return template, cpds, b, s
 
-np.random.seed(1234)
-random.seed(1234)
+#np.random.seed(1234)
+#random.seed(1234)
 
 N = 5
-iters = 3e5
-numdata = 500
-priorweight = 10
+iters = 1e4
+numdata = 50
+priorweight = 1
 numtemplate = 3
 db = 'memory'
 
 graph = generateHourGlassGraph(nodes=N)
+gmat = np.asarray(nx.to_numpy_matrix(graph))
 
-fname = 'trash.h5'
-if os.path.exists(fname):
-  os.remove(fname)
+def indiv_edge_presence(net):
+  return net['matrix'][net['x'][0],net['x'][3]]
+
+def global_edge_presence(net):
+  s = net['x'].argsort()
+  ordmat = net['matrix'][s].T[s].T
+  return np.abs(gmat - ordmat).sum() / net.shape[0]**2
 
 template = sampleTemplate(graph, numtemplate)
 tmat = np.asarray(nx.to_numpy_matrix(template))
 traindata, states, cpds = generateData(graph,numdata)
 nodes = np.arange(graph.number_of_nodes())
 
-b,s = runSAMC(states, traindata, tmat, db)
-b2,s2 = runSAMC(states, traindata, tmat, db)
+b,s = runSAMC(states, traindata, tmat, db, global_edge_presence)
+#b2,s2 = runSAMC(states, traindata, tmat, db)
 
 #temp2,_,b2,s2 = runFullSAMC(graph, iters, numtemplate, numdata, priorweight, 'memory')
 #temp,_,b,s = runFullSAMC(graph, iters, numtemplate, numdata, priorweight, 'trash.h5')
 
 #sys.exit()
 
-b.update_graph(s.mapvalue)
-b2.update_graph(s2.mapvalue)
-drawGraphs(graph, template, b.graph, b2.graph)
+#b.update_graph(s.mapvalue)
+#b2.update_graph(s2.mapvalue)
+#drawGraphs(graph, template, b.graph, b2.graph)
 
 #dataset = to_pebl(states, traindata)
 
