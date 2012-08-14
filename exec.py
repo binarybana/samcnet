82ff0922ba9cc9b4cb67ee2e2265421a0795c3f4
Jason Knight <Jason@JasonKnight.us>
2012-08-14 15:58:31 -0500

    Removed execnet in favor of ssh+redis
    Now instead of controlling everything through redis, the exec.py driver
    will launch instances of py/driver.py remotely, which then poll and are
    controlled by the master redis db. The redis db is again controlled by
    exec.py.

--- exec.py
+++ exec.py
@@ -1,47 +1,103 @@
 #!/usr/bin/env python
-import execnet as ex
-import os, sys, subprocess, shlex
-from collections import namedtuple
+import os, sys, shlex, time, sha
+import subprocess as sb
+import redis
+import random
+from collections import namedtuple, Counter
 
 ServerConfig = namedtuple('ServerConfig', 'hostname root python cde cores') 
 SyncGroup = namedtuple('SyncGroup', 'hostname dir cde')
 
 LocalRoot = '/home/bana/GSP/research/samc/code'
 
-def makeGateway(host):
+def launchClient(host):
+  cores = host.cores
   if host.cde:
-    spec = ('ssh={0.hostname}//chdir={0.root}/cde-package/cde-root/' + \
-        'home/bana/GSP/research/samc/code/py//python={0.python}').format(host)
+    spec = ('ssh {0.hostname} cd {0.root}/cde-package/cde-root/' \
+        + 'home/bana/GSP/research/samc/code; ').format(host) \
+        + ('{0.python} py/driver.py &'.format(host))*cores
   else:
-    spec = ('ssh={0.hostname}//chdir={0.root}/py//python={0.python}').format(host)
+    spec = ('ssh {0.hostname} cd {0.root};'.format(host) \
+        + ('LD_LIBRARY_PATH=./build:$LD_LIBRARY_PATH {0.python} py/driver.py &'.format(host))*cores)
 
-  print "Connecting to: "
-  for x in spec.split('//'):
-    print '\t'+x
-  gw = ex.makegateway(spec)
-  return gw
+  print "Connecting to %s." % host.hostname
+  sb.Popen(shlex.split(spec), 
+      bufsize=-1,
+      stdout=open('/tmp/samc-{0.hostname}-{1}.log'.format(host, random.randint(0,1e9)) ,'w'))
+  return 
 
 def sync(group):
   if group.cde:
-    updateCDE()
-    p = subprocess.Popen('rsync -aczP --delete {0}/cde-package {1.hostname}:{1.dir}'.format(LocalRoot, group).split())
+    print "Beginning rsync to %s... " % group
+    p = sb.Popen('rsync -acz {0}/cde-package {1.hostname}:{1.dir}'.format(LocalRoot, group).split())
   else:
-    p = subprocess.Popen('rsync -aczP --delete --exclude=*cde* --exclude=build {0}/ {1.hostname}:{1.dir}/'.format(LocalRoot, group).split())
+    print "Beginning rsync... "
+    p = sb.Popen('rsync -acz --exclude=*cde* --exclude=build {0}/ {1.hostname}:{1.dir}/'.format(LocalRoot, group).split())
+    print ' Done.'
     p.wait()
-    p = subprocess.Popen(shlex.split('ssh {0.hostname} "cd {0.dir}; ./waf distclean; . cfg; ./waf"'.format(group)))
-  return p.wait()
+    print 'Beginning remote rebuild...'
+    p = sb.Popen(shlex.split('ssh {0.hostname} "cd {0.dir}; ./waf distclean; . cfg; ./waf"'.format(group)))
+  p.wait()
+  print ' Done.'
 
 def updateCDE():
+  print "Updating CDE package..." 
   os.environ['LD_LIBRARY_PATH']='./build:../build'
   os.chdir(LocalRoot)
-  p = subprocess.Popen('/home/bana/bin/cde python {0}/py/driver.py'.format(LocalRoot).split())
+  p = sb.Popen('/home/bana/bin/cde python {0}/py/driver.py'.format(LocalRoot).split())
+  time.sleep(1)
+  p2 = sb.Popen('./exec.py postdummy'.split())
+  p2.wait()
+  p2 = sb.Popen('./exec.py kill {0}'.format(os.uname()[1].split('.')[0]).split())
+  p2.wait()
   p.wait()
+  print " Done."
+
+def postJob(job, samples):
+  numcl = r.llen('clients-alive')
+  if numcl == 0:
+    print "No clients online, aborting."
+  else:
+    h = sha.sha(job).hexdigest()
+    tot = r.hincrby('desired-samples', h, samples)
+    r.hsetnx('configs', h, job)
+    num = r.llen('clients-alive')
+    print("Added %d samples for a total of %d samples remaining." % (samples,tot))
+    print("Pushed to %d clients. Job:" % num)
+    for x in job.split():
+      print '\t'+x
+
+def kill(target):
+  assert not r.exists('die')
+  print "Sending out kill command to %s." % target
+  r.set('die', target)
+
+  def countTarget(target):
+    if target == 'all':
+      return r.llen('clients-alive')
+    else:
+      return len([x for x in r.lrange('clients-alive', 0, -1) if x == target])
+
+  try:
+    tot = num = countTarget(target)
+    print ('Waiting for %d clients to die...' % num)
+
+    while num > 0:
+      time.sleep(1)
+      num = countTarget(target)
+
+    print("%d clients killed." % tot)
+  finally:
+    r.delete('die')
 
 if len(sys.argv) < 2:
-  print "Usage: python exec.py [synchost <hostname>] [syncall] [testrun <hostname>] [run]"
+  print "Usage: python exec.py [sync <groupname>] [syncall]"+\
+      " [postdummy] [kill <hostname>] [killall] [status]"
   sys.exit(-1)
 
 goal = sys.argv[1]
+
+r = redis.StrictRedis('knight-server.dyndns.org')
 
 syncgroups = {'cesg': SyncGroup('hornet', '/home/bana', True),
     'gsp': SyncGroup('kubera', '/home/jason/samc/samcsynthetic-0.2', False),
@@ -50,20 +106,23 @@ syncgroups = {'cesg': SyncGroup('hornet', '/home/bana', True),
 
 serverconfigs = {}
 
-for x in 'mustang spitfire kingcobra zero'.split():
+cesg_small = 'mustang spitfire kingcobra zero'.split()
+for x in cesg_small:
   serverconfigs[x] = ServerConfig(x, 
       '/home/bana',  
       '/home/bana/cde-package/cde-exec python', 
       True,
-      8)
+      4)
+cesg_large = 'raptor blackbird hornet'.split()
+for x in cesg_large:
+  serverconfigs[x] = ServerConfig(x, 
+      '/home/bana',  
+      '/home/bana/cde-package/cde-exec python', 
+      True,
+      20)
 
-for x in 'raptor blackbird hornet'.split():
-  serverconfigs[x] = ServerConfig(x, 
-      '/home/bana',  
-      '/home/bana/cde-package/cde-exec python', 
-      True,
-      47)
-for x in ['compute-0-1', 'compute-0-2', 'compute-0-3', 'kubera']:
+gsp_compute = ['compute-0-%d'%x for x in range(1,4)]
+for x in  gsp_compute + ['kubera']:
   serverconfigs[x] = ServerConfig(x, 
       '/home/jason/samc/samcsynthetic-0.2', 
       '/share/apps/bin/python2.7', 
@@ -79,11 +138,18 @@ serverconfigs['sequencer'] = ServerConfig('sequencer',
       '/home/jason', 
       '/home/jason/cde-package/cde-exec python', 
       True,
-      12)
+      10)
+serverconfigs['bana-desktop'] = ServerConfig('bana-desktop', 
+      '/home/bana/GSP/research/samc/code', 
+      'python', 
+      False,
+      2)
 
-if goal == 'syncgroup':
+if goal == 'sync':
   assert sys.argv[2] in syncgroups
   group = syncgroups[sys.argv[2]]
+  if group.cde:
+    updateCDE()
   #rsync to group
   sync(group)
 
@@ -92,22 +158,57 @@ elif goal == 'syncall':
   for x in syncgroups.values():
     sync(x)
 
-elif goal == 'testrun':
+elif goal == 'launch':
   assert sys.argv[2] in serverconfigs
   host = serverconfigs[sys.argv[2]]
-  sys.path.append('../code/py')
-  import driver
 
-  mch = ex.MultiChannel([makeGateway(host).remote_exec(driver) for x in range(host.cores)])
+  launchClient(host)
 
-  #mch = ex.MultiChannel([makeGateway(host).remote_exec("channel.send(channel.receive()+1)") for x in range(host.cores)])
-  #mch.send_each(1)
+elif goal == 'launchgroup':
+  for host in cesg_small:
+  #for host in gsp_compute + 'toxic sequencer bana-desktop'.split():
+  #for host in gsp_compute + cesg_small + 'raptor toxic sequencer bana-desktop'.split():
+    cfg = serverconfigs[host]
+    time.sleep(0.2)
+    launchClient(cfg)
 
-  #ch = gw.remote_exec(driver)
-  #ch = gw.remote_exec("channel.send(channel.receive()+1)")
+elif goal == 'postdummy':
+  test = """
+[General]
+nodes = 15
+samc-iters=5e5
+numdata=500
+priorweight=10
+numtemplate=5"""
+  postJob(test, samples=20)
 
-  print mch.receive_each()
+elif goal == 'killall':
+  kill('all')
 
-elif goal == 'run':
-  pass
-  #Run all
+elif goal == 'kill':
+  assert sys.argv[2] in serverconfigs
+  kill(sys.argv[2])
+
+elif goal == 'status':
+  r.sort('clients-alive')
+  clients = r.lrange('clients-alive', 0, -1)
+  num = len(clients)
+  if num == 0:
+    print('There are currently no clients alive.')
+  else:
+    print("The %d clients alive are:" % num)
+    cores = Counter(clients)
+    clients = list(set(clients))
+    clients.sort()
+    for x in clients:
+      print '\t%3d cores on %s' %(cores[x],x)
+  print("The job list is currently:")
+  joblist = r.hgetall('desired-samples')
+  for i,x in joblist.iteritems():
+    #print '\t%s\t%s' % (r.hget('configs',i),x)
+    print '\t%s: %s' % (i,x)
+
+  print 'Current sample counts:'
+  for x in joblist.keys():
+    print '\t%s: %3d' % (x,r.llen(x))
+
--- py/driver.py
+++ py/driver.py
@@ -1,68 +1,100 @@
-if __name__ == '__channelexec__' or __name__=='__main__':
-    import sys, os, io
-    import numpy as np
-    import networkx as nx
-    import ConfigParser as cp
+import sys, os, io
+import redis
+import sha
+import atexit
+import numpy as np
+import networkx as nx
+import ConfigParser as cp
 
-    from time import time
+from time import time, sleep
 
-    sys.path.append('../build')
-    sys.path.append('./build') # Yuck!
+sys.path.append('../build')
+sys.path.append('./build') # Yuck!
 
-    from samc import SAMCRun
-    from bayesnet import BayesNet
-    from generator import *
+try:
+  from samc import SAMCRun
+  from bayesnet import BayesNet
+  from generator import *
+except ImportError as e:
+  print(e)
+  print("Make sure LD_LIBRARY_PATH is set correctly and that the build"+\
+      " directory is populated by waf.")
+  sys.exit()
 
-    #Just for now...
-    test = """
-[General]
-nodes = 5
-samc-iters=5e5
-numdata=50
-priorweight=10
-numtemplate=5
-    """
-    os.environ['SAMC_CONFIG'] = test
-    print test.encode('ascii')
+r = redis.StrictRedis('knight-server.dyndns.org')
 
-    config = cp.RawConfigParser()
-    config.readfp(io.BytesIO(os.environ['SAMC_CONFIG']))
+def getHost():
+  return os.uname()[1].split('.')[0]
 
-    N = config.getfloat('General', 'nodes')
-    iters = config.getfloat('General', 'samc-iters')
-    numdata = config.getint('General', 'numdata')
-    priorweight = config.getfloat('General', 'priorweight')
-    numtemplate = config.getint('General', 'numtemplate')
-    #db = config.get('General', 'db')
+def recordDeath():
+  r.lrem('clients-alive', 1, getHost())
 
-    graph = generateHourGlassGraph(nodes=N)
-    gmat = np.asarray(nx.to_numpy_matrix(graph))
+atexit.register(recordDeath)
+r.rpush('clients-alive', getHost())
+print("Registered with db.")
 
-    def global_edge_presence(net):
-        s = net['x'].argsort()
-        ordmat = net['matrix'][s].T[s].T
-        return np.abs(gmat - ordmat).sum() / net['x'].shape[0]**2
+while True:
+  cmd = r.get('die')
+  if cmd == 'all' or cmd == getHost():
+    print("Received die command, shutting down.")
+    break
 
-    template = sampleTemplate(graph, numtemplate)
-    tmat = np.asarray(nx.to_numpy_matrix(template))
-    traindata, states, cpds = generateData(graph,numdata)
-    nodes = np.arange(graph.number_of_nodes())
+  with r.pipeline() as pipe:
+    queue = r.hgetall('desired-samples')
+    workhash = None
+    for h,num in queue.iteritems():
+      if int(num) > 0:
+        print("\nFound %s samples left on hash %s" % (num, h))
+        workhash = h
+    if workhash != None:
+      r.hincrby('desired-samples', workhash, -1)
 
-    nodes = np.arange(traindata.shape[1])
-    b = BayesNet(nodes,states,traindata,template=tmat)
-    s = SAMCRun(b)
+  if workhash == None:
+    print 'sleep... '
+    sleep(2)
+    continue
 
-    t1 = time()
-    s.sample(iters)
-    t2 = time()
-    print("SAMC run took %f seconds." % (t2-t1))
-    func_mean = s.estimate_func_mean(global_edge_presence)
-    t3 = time()
-    print("Mean estimation run took %f seconds." % (t3-t2))
+  job = r.hget('configs', workhash)
 
-    # Send back func_mean to store
-    if __name__ == '__channelexec__':
-      channel.send(float(func_mean))
-    else:
-      print(func_mean)
+  ########## Read config from Redis ########
+  config = cp.RawConfigParser()
+  config.readfp(io.BytesIO(job))
+
+  N = config.getfloat('General', 'nodes')
+  iters = config.getfloat('General', 'samc-iters')
+  numdata = config.getint('General', 'numdata')
+  priorweight = config.getfloat('General', 'priorweight')
+  numtemplate = config.getint('General', 'numtemplate')
+
+  ########### Actual simulation ############
+  graph = generateHourGlassGraph(nodes=N)
+  gmat = np.asarray(nx.to_numpy_matrix(graph))
+
+  def global_edge_presence(net):
+      s = net['x'].argsort()
+      ordmat = net['matrix'][s].T[s].T
+      return np.abs(gmat - ordmat).sum() / net['x'].shape[0]**2
+
+  template = sampleTemplate(graph, numtemplate)
+  tmat = np.asarray(nx.to_numpy_matrix(template))
+  traindata, states, cpds = generateData(graph,numdata)
+  nodes = np.arange(graph.number_of_nodes())
+
+  nodes = np.arange(traindata.shape[1])
+  b = BayesNet(nodes,states,traindata,template=tmat)
+  s = SAMCRun(b)
+
+  t1 = time()
+  s.sample(iters)
+  t2 = time()
+  print("SAMC run took %f seconds." % (t2-t1))
+  func_mean = s.estimate_func_mean(global_edge_presence)
+  t3 = time()
+  print("Mean estimation run took %f seconds." % (t3-t2))
+  ########### ################# ############
+
+  # Send back func_mean to store
+  r.lpush(workhash, func_mean)
+  print('Function mean: %f' % func_mean)
+
 
