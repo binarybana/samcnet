662f9ee41dc7574348f7b5d28a2707371fe604fd
Jason Knight <Jason@JasonKnight.us>
2012-12-04 14:11:10 -0600

    Updated tail to use proper priors
    Found out that there is a few missing constants in the analytic solution
    of tail.py effective densities (due to differences between inverse
    wishart and gamma). I still need to find the values of the actual
    constants, but as can be seen by the ludicrously close match between the
    gavg and sampled predictive posterior, the method is working correctly.

--- samcnet/tail.py
+++ samcnet/tail.py
@@ -28,7 +28,7 @@ class Classification():
     def __init__(self):
         np.random.seed(1234)
 
-        self.n = 1 # Data points
+        self.n = 4 # Data points
 
         self.true_mu = 0.0
         self.true_sigma = 1 #di.invgamma.rvs(3)
@@ -41,7 +41,9 @@ class Classification():
         self.gavg = np.zeros(self.grid_n)
         self.numgavg = 0
 
-        self.data = di.norm.rvs(size=self.n)
+        #self.data = di.norm.rvs(size=self.n)
+        self.data = np.array([0.0, -0.0, 0.5, -0.5])
+        assert self.data.size == self.n
         
         ######## Starting point of MCMC Run #######
         self.mu = 0.0
@@ -50,6 +52,41 @@ class Classification():
         ###### Bookeeping ######
         self.oldmu = None
         self.oldsigma = None
+
+        ##### Prior Values and Confidences ######
+        self.priorsigma = 2
+        self.kappa = 1
+        self.priormu = 0
+        self.nu = 8.0
+        #### Calculating the Analytic solution given on page 15 of Lori's 
+        #### Optimal Classification eq 34.
+        self.nustar = self.nu + self.n
+
+        samplemean = self.data.mean()
+        samplevar = np.cov(self.data)
+
+        self.mustar = (self.nu*self.priormu + self.n * samplemean) \
+                / (self.nu + self.n)
+        self.kappastar = self.kappa + self.n
+        self.Sstar = self.priorsigma + (self.n-1)*samplevar + self.nu*self.n/(self.nu+self.nu)\
+                * (samplemean - self.priormu)**2
+                
+        #### Now calculate effective class conditional densities from eq 55
+        #### page 21
+
+        #self.fx = MVT(
+                #self.mu0star, 
+                #(self.nu0star+1)/(self.kappa0star-self.D+1)/self.nu0star * self.S0star, 
+                #self.kappa0star - self.D + 1)
+        # So I'm pretty sure this is incorrect below, off by some scaling
+        # parameters
+        self.fx = MVT(
+                [self.mustar], 
+                [(self.nustar+1)/(self.kappastar)/self.nustar * self.Sstar / 2],
+                self.kappastar /2 )
+
+        self.analyticfx = self.fx.logpdf(self.grid.reshape(-1,1))
+
 
     def propose(self):
         self.oldmu = self.mu
@@ -70,36 +107,35 @@ class Classification():
     def energy(self):
         sum = 0.0
         sum -= di.norm.logpdf(self.data, loc=self.mu, scale=self.sigma).sum()
-
         #Now add in the priors...
-        #sum -= di.invgamma.logpdf(self.sigma, self.priorgamma)
-        sum -= log(self.sigma)*-2 #di.norm.logpdf(self.mu, loc=self.priormu, scale=self.priortau)
+        sum -= log(self.sigma)*(-0.5) - self.nu/2 * (self.mu-self.priormu)**2/self.sigma
+        sum -= log(self.sigma)*(self.kappa+2)/(-2) - 0.5*self.priorsigma/self.sigma
         return sum
 
     def calc_gfunc(self):
         return di.norm.pdf(self.grid, loc=self.mu, scale=self.sigma) 
 
     def init_db(self, db, dbsize):
-        dtype = [('thetas',np.double),
-                ('energies',np.double),
-                ('funcs',np.double)]
-        if db == None:
-            return np.zeros(dbsize, dtype=dtype)
-        elif db.shape[0] != dbsize:
-            return np.resize(db, dbsize)
-        else:
-            raise Exception("DB Not inited")
+        pass
+        #dtype = [('thetas',np.double),
+                #('energies',np.double),
+                #('funcs',np.double)]
+        #if db == None:
+            #return np.zeros(dbsize, dtype=dtype)
+        #elif db.shape[0] != dbsize:
+            #return np.resize(db, dbsize)
+        #else:
+            #raise Exception("DB Not inited")
 
     def save_to_db(self, db, theta, energy, iteration):
-        func = 0.0
-        db[iteration] = np.array([theta, energy, func])
+        #func = 0.0
+        #db[iteration] = np.array([theta, energy, func])
         global mydb
         mydb.append(self.copy())
 
         # Update G function average
         self.numgavg += 1
         self.gavg += (self.calc_gfunc() - self.gavg) / self.numgavg
-        #mydb.append(self.calc_gfunc())
 
 def pnorm(loc,scale):
     p.figure()
@@ -108,63 +144,92 @@ def pnorm(loc,scale):
     p.show()
 
 if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    import matplotlib.animation as animation
+
     import samc
     from samcnet.utils import *
     c = Classification()
 
-    p.close('all')
+    #p.close('all')
     s = MHRun(c, burn=0)
     #s = samc.SAMCRun(c, burn=0, stepscale=1000, refden=0)
-    s.sample(5e4)
-    plotHist(s)
-    p.subplot(4,1,1)
-    p.plot(c.grid, c.gavg, 'r')
-    p.plot(c.data, np.ones_like(c.data), 'ko')
-    p.grid(True)
-    x = np.linspace(0.01,2,50)
-    p.subplot(4,1,2)
-    p.hist(np.vstack(mydb)[:,1],bins=x)
+    s.sample(1e3)
+    #plotHist(s)
 
-    p.subplot(4,1,3)
-    mus = np.vstack(mydb)[:,0]
-    counts,bins,_ = p.hist(mus,bins=80)
+    ##################################
+    ##################################
+    #p.subplot(4,1,1)
+    #p.plot(c.grid, c.gavg, 'r')
+    #p.plot(c.data, np.ones_like(c.data), 'ko')
+    #p.grid(True)
+    #x = np.linspace(0.01,2,50)
+    #p.subplot(4,1,2)
+    #p.hist(np.vstack(mydb)[:,1],bins=x)
 
-    xx = np.linspace(bins[0], bins[-1], 300)
-    ty = di.t.pdf(xx, *di.t.fit(mus))
-    ny = di.norm.pdf(xx, *di.norm.fit(mus))
+    #p.subplot(4,1,3)
+    #mus = np.vstack(mydb)[:,0]
+    #counts,bins,_ = p.hist(mus,bins=80)
 
-    p.plot(xx,ty*counts.max()/ty.max(),'g', label='t fit')
-    p.plot(xx,ny*counts.max()/ny.max(),'b--', label='normal fit')
-    p.legend()
+    #xx = np.linspace(bins[0], bins[-1], 300)
+    #ty = di.t.pdf(xx, *di.t.fit(mus))
+    #ny = di.norm.pdf(xx, *di.norm.fit(mus))
 
-    p.subplot(4,1,4)
-    ys = np.vstack(mydb)[:,2]
-    counts,bins,_ = p.hist(ys,bins=80)
+    #p.plot(xx,ty*counts.max()/ty.max(),'g', label='t fit')
+    #p.plot(xx,ny*counts.max()/ny.max(),'b--', label='normal fit')
+    #p.legend()
 
-    xx = np.linspace(bins[0], bins[-1], 300)
-    ty = di.t.pdf(xx, *di.t.fit(ys))
-    ny = di.norm.pdf(xx, *di.norm.fit(ys))
+    #p.subplot(4,1,4)
+    #ys = np.vstack(mydb)[:,2]
+    #counts,bins,_ = p.hist(ys,bins=80)
 
-    p.title("sampled y's")
-    p.plot(xx,ty*counts.max()/ty.max(),'g', label='t fit')
-    p.plot(xx,ny*counts.max()/ny.max(),'b--', label='normal fit')
-    p.legend()
+    #xx = np.linspace(bins[0], bins[-1], 300)
+    #ty = di.t.pdf(xx, *di.t.fit(ys))
+    #ny = di.norm.pdf(xx, *di.norm.fit(ys))
+    #ay = c.fx.pdf(xx.reshape(-1,1))
 
-    p.figure()
-    p.title('logpdfs')
-    xx = np.linspace(bins[0], bins[-1], 300)
-    ty = di.t.logpdf(xx, *di.t.fit(ys))
-    ny = di.norm.logpdf(xx, *di.norm.fit(ys))
+    #p.title("sampled y's")
+    #p.plot(xx,ty*counts.max()/ty.max(),'g', label='t fit')
+    #p.plot(xx,ny*counts.max()/ny.max(),'b--', label='normal fit')
+    
+    #p.plot(xx,ay*counts.max()/ay.max(),'k--', label='t analytic')
 
-    p.title("sampled y's")
-    p.plot(xx,ty,'g', label='t empirical')
-    p.plot(xx,ny,'b--', label='normal empirical')
+    #p.legend()
 
-    p.plot(c.grid, np.log(c.gavg), 'r', label='gavg')
+    ##############################
+    fig1 = plt.figure()
+    #xx = np.linspace(bins[0], bins[-1], 300)
+    #ty = di.t.logpdf(xx, *di.t.fit(ys))
+    #p.plot(xx,ty,'g', label='t empirical')
 
-    p.plot(c.data, np.ones_like(c.data), 'ko')
-    p.grid(True)
-    p.legend()
+    plt.title("predictive posteriors")
+    plt.ylabel('logpdfs')
+    plt.grid(True)
+    plt.hold(True)
 
-    p.show()
+    plt.plot(c.data, np.ones_like(c.data), 'ko', label='data')
+    plt.plot(c.grid, np.exp(c.analyticfx), 'k--', label='student t')
+    if True:
+        s.sample(3e3)
+        plt.plot(c.grid, c.gavg, 'r', label='gavg')
+        ys = np.vstack(mydb)[:,2]
+        counts,bins,_ = p.hist(ys,bins=80,normed=True)
+
+    elif False: # Animation
+        l, = plt.plot(c.grid, c.gavg, 'r', label='gavg')
+        def update_line(num, data, line):
+            global c
+            line.set_data(c.grid,data[num])
+            return line,
+        data = [c.gavg.copy()]
+        N=int(sys.argv[1])
+        for x in range(N):
+            s.sample(1e3)
+            data.append(c.gavg.copy())
+
+        line_ani = animation.FuncAnimation(fig1, update_line, N, fargs=(data, l),
+            interval=50, blit=True)
+
+    plt.show()
+
 
