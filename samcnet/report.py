f61607cee7cb43547269088aad5bd138548add42
Jason Knight <Jason@JasonKnight.us>
2013-01-31 13:42:35 -0600

    Allow plotting arbitrary funcs in report

--- samcnet/report.py
+++ samcnet/report.py
@@ -35,25 +35,17 @@ datastrings = r.lrange(done_hashes[sel], 0, -1)
 
 for d in datastrings:
     res = [decode_element(x) for x in load_data(d)]
-    entropy_mean = res[0]
-    kld_mean = res[2]
-    edge_mean = res[4]
-    entropy_cummean = res[1]
-    kld_cummean = res[3]
-    edge_cummean = res[5]
 
-    #print("KLD Mean is: %s" % kld_mean)
-    #print("Entropy Mean is: %s" % entropy_mean)
-
-    ent = p.plot(entropy_cummean, 'r', alpha=0.3, label='Entropy')
-    kld = p.plot(kld_cummean, 'g', alpha=0.3, label='KLD from true')
-    edge = p.plot(edge_cummean, 'b', alpha=0.3, label='Edge dist from true')
+    n = len(res)
+    indices = np.linspace(0,1,n)
+    for i,val in enumerate(res):
+        if type(val) == np.ndarray:
+            p.plot(val, alpha=0.4, color=p.cm.jet(indices[i]))
+            print indices[i], p.cm.jet(indices[i])
 
 p.ylim(0, 7)
 p.xlabel('Iterations after burnin')
-p.ylabel('nats')
 p.title(r.hget('jobs:descs', done_hashes[sel][10:]))
 p.grid(True)
-p.legend([ent[0], kld[0], edge[0]], ["Entropy", "KLD from true", "Edge distance"], loc='best')
 
 p.show()
