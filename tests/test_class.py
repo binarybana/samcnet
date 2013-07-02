286117567840c4f3e0c68433a3fe8d1aab6f2230
Jason Knight <Jason@JasonKnight.us>
2013-07-02 18:13:26 -0500

    Setup test/test_net.py for zmq, still not working though

--- tests/test_class.py
+++ tests/test_class.py
@@ -3,26 +3,40 @@ import sys
 import redis
 import random
 import numpy as np
+
 try:
     from samcnet import samc,lori,utils
+    from samcnet.lori import *
 except ImportError as e:
     sys.exit("Make sure LD_LIBRARY_PATH is set correctly and that the build"+\
             " directory is populated by waf.\n\n %s" % str(e))
 
 if 'WORKHASH' in os.environ:
     try:
-        redis_server = os.environ['REDIS']
-        import redis
-        r = redis.StrictRedis(redis_server)
+        server = os.environ['SERVER']
     except:
-        sys.exit("ERROR in worker: Need REDIS environment variable defined.")
+        sys.exit("ERROR in worker: Need SERVER environment variable defined.")
 
-c = lori.Johnson()
-#c = lori.Classification()
-s = samc.SAMCRun(c, burn=1e3, stepscale=10000, refden=2, thin=10)
-s.sample(2e4, temperature=10)
+## First generate true distributions and data
+cval, dist0, dist1 = gen_dists()
 
-s.compute_means()
+## Now test Gaussian Analytic calculation
+gc = GaussianCls(dist0, dist1)
+
+c = GaussianSampler(dist0,dist1)
+s = samc.SAMCRun(c, burn=0, stepscale=1000, refden=1, thin=10)
+s.sample(1e2, temperature=1)
 
 if 'WORKHASH' in os.environ:
-    r.lpush('jobs:done:'+os.environ['WORKHASH'], s.read_db())
+    import zmq,time
+    ctx = zmq.Context()
+    socket = ctx.socket(zmq.PUSH)
+    socket.connect('tcp://'+server+':7000')
+
+    data = s.read_db()
+    socket.send(os.environ['WORKHASH'], zmq.SNDMORE)
+    socket.send(data)
+    time.sleep(2)
+    socket.close()
+    ctx.term()
+
