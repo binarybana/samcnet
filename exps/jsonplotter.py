bbfdfe8460b8f87608e57aedcaa70dea5d009ae5
Jason Knight <Jason@JasonKnight.us>
2014-01-14 11:04:05 -0600

    Update plotting

--- exps/jsonplotter.py
+++ exps/jsonplotter.py
@@ -1,16 +1,22 @@
-import os
+import os, sys
 import numpy as np
-import pylab as p
 import simplejson as js
 import pandas as pa
 import yaml
+
+import matplotlib as mpl
+mpl.use('Agg')
+import pylab as p
 
 from collections import defaultdict
 
 from jobmon import redisbackend as rb
 
+p.rc('font', size=11)
+
 db = rb.RedisDataStore('localhost')
 jobhash = db.select_jobfile()
+name = raw_input("Name: ")
 
 resdir = os.path.join('/home/bana/largeresearch/results', jobhash)
 dependent_var = []
@@ -40,11 +46,16 @@ for i,paramdir in enumerate(os.listdir(resdir)):
     print(otherdf.describe())
 
 dependent_var = np.array(dependent_var)
-ind = np.argsort(dependent_var)
+ind = np.argsort(dependent_var)#[1:]
 markers = list('xD*o>s^<+')
+colors = list('bgrcmy')
 offset = 0
-key = {'gauss':'Normal OBC', 'svm':'SVM', 'knn':'3NN', 'lda':'LDA', 'mpm':'MP OBC',
+key = {'gauss':'Normal OBC', 'svm':'RBF SVM', 'knn':'3NN', 'lda':'LDA', 'mpm':'MP OBC',
         'mpm_prior':'MP OBC Prior', 'mpmc_calib':'MP OBC Calibrated'}
+colorkey = {'gauss':'b', 'svm':'y', 'knn':'g', 'lda':'r', 'mpm':'c',
+        'mpm_prior':'k', 'mpmc_calib':'m'}
+symkey = {'gauss':'x', 'svm':'^', 'knn':'D', 'lda':'*', 'mpm':'o',
+        'mpm_prior':'+', 'mpmc_calib':'s'}
 
 def adjust_plot():
     lx,ux,ly,uy = p.axis()
@@ -53,40 +64,55 @@ def adjust_plot():
     p.axis((lx-xl*0.1, ux+xl*0.1, ly-yl*0.1, uy+yl*0.1))
 
 #p.close('all')
+df.sort_index(axis=1)
 
 offset = 0
 p.figure()
 for i in range(len(df.columns)):
+    if df.columns[i] == 'mpm_prior' or df.columns[i] == 'gauss':
+        continue
     means = [alldata[j].mean()[i] for j in dependent_var[ind]]
     stds = [alldata[j].std()[i] for j in dependent_var[ind]]
-    p.plot(dependent_var[ind], means, marker=markers[i], markersize=10, label=key[df.columns[i]])
+    p.plot(dependent_var[ind], means, marker=symkey[df.columns[i]], color=colorkey[df.columns[i]], 
+            markersize=7, label=key[df.columns[i]])
     #p.errorbar(dependent_var[ind]+offset, means, yerr=stds, marker='o',
             #label=key[df.columns[i]])
     #offset += 0.1
 p.legend()
-p.ylabel('Mean holdout error')
+#p.ylim(0.30, 0.40) # For TCGA
+#p.ylim(0.40, 0.46) # For HC
+p.ylabel('Mean separate sampling holdout error')
+#p.ylabel('Mean estimated true error')
 p.xlabel('Training samples per class')
+#p.xlabel('Total training samples')
 #p.xlabel(r'$\mu_1$')
 #p.xlabel('Gene expression \"strength\"')
 p.grid(True)
 adjust_plot()
 
-#p.figure()
-#for i in range(len(df.columns)):
-    #if df.columns[i] == 'mpm':
-        #continue
-    #diffs = [alldata[j].iloc[:,i]-alldata[j].loc[:,'mpm'] for j in dependent_var[ind]]
-    #diffdf = pa.concat(diffs, keys=dependent_var[ind], axis=1)
-    #p.errorbar(dependent_var[ind]+offset, diffdf.mean(), yerr=diffdf.std(), marker='o',
-            #label=key[df.columns[i]])
-    #offset += 0.01
-#p.legend()
-#p.ylabel('Holdout error - Holdout error for MP OBC')
-#p.xlabel('Training samples per class')
-#p.grid(True)
-#adjust_plot()
+savename = '../class-paper/pdf/{}-{}.pdf'.format(name, jobhash[:4])
+print('Saving to {}'.format(savename))
+p.savefig(savename, bbox_inches='tight')
 
-p.title(jobhash[:6] + ' ' + db.get_description(jobhash))
+#p.show()
+sys.exit()
+
+p.figure()
+for i in range(len(df.columns)):
+    if df.columns[i] == 'mpm' or df.columns[i] == 'mpm_prior':
+        continue
+    diffs = [alldata[j].iloc[:,i]-alldata[j].loc[:,'mpm'] for j in dependent_var[ind]]
+    diffdf = pa.concat(diffs, keys=dependent_var[ind], axis=1)
+    p.errorbar(dependent_var[ind]+offset, diffdf.mean(), yerr=diffdf.std(), marker='o',
+            label=key[df.columns[i]])
+    offset += 0.01
+p.legend()
+p.ylabel('Holdout error - Holdout error for MP OBC')
+p.xlabel('Total training samples per class')
+p.grid(True)
+adjust_plot()
+
+#p.title(jobhash[:6] + ' ' + db.get_description(jobhash))
 #p.xlabel('Number of final features')
 #p.xlabel('mu1')
 
